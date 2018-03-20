6d44cead7140979aefe7c4743e97170cd9ab5f37
roberrera <roberterrera@gmail.com>
2018-03-20 00:29:10 -0400

    Add a send button and add a theme

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,8 @@ class FriendlychatApp extends StatelessWidget {
 class ChatScreen extends StatefulWidget {
   @override
   State createState() => new ChatScreenState();
+  // In Dart syntax, the fat arrow function declaration => expression is
+  // shorthand for { return expression; }.
 }
 
 // Add the ChatScreenState class definition in main.dart.
@@ -25,13 +27,28 @@ class ChatScreenState extends State<ChatScreen> {
   final TextEditingController _textController = new TextEditingController();
 
   Widget _buildTextComposer() {
-    return new Container(
+    return new IconTheme(
+      data: new IconThemeData(color: Theme.of(context).accentColor),
+      child: new Container(
       margin: const EdgeInsets.symmetric(horizontal: 8.0),
-      child: new TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: new InputDecoration.collapsed(
-            hintText: "Send a message"),
+        child: new Row(
+          children: <Widget>[
+            new Flexible(
+              child: new TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration: new InputDecoration.collapsed(
+                  hintText: "Send a message"),
+              ),
+            ),
+            new Container(
+              margin: new EdgeInsets.symmetric(horizontal: 4.0),
+              child: new IconButton(
+                icon: new Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text)),
+            ),
+          ],
+        ),
       ),
     );
   }
