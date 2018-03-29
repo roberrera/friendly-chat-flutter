369a64277bdfbec5a0e685d8436e9daf94822a34
roberrera <roberterrera@gmail.com>
2018-03-28 23:33:11 -0400

    Update the Send Message UI and move to bottom of screen

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,45 @@ void main() {
   runApp(new FriendlychatApp());
 }
 
+const String _name = "Rob";
+
 class FriendlychatApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: "Friendlychat",
       home: new ChatScreen(),
+    );
+  }
+}
+
+class ChatMessage extends StatelessWidget {
+  ChatMessage({this.text});
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(vertical: 10.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          new Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: new CircleAvatar(child: new Text(_name[0])),
+          ),
+          new Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              new Text(_name, style: Theme.of(context).textTheme.subhead),
+              new Container(
+                margin: const EdgeInsets.only(top: 5.0),
+                child: new Text(text),
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
@@ -21,24 +54,32 @@ class ChatScreen extends StatefulWidget {
   // shorthand for { return expression; }.
 }
 
-// Add the ChatScreenState class definition in main.dart.
 class ChatScreenState extends State<ChatScreen> {
   // Prefixing an identifier with an _ (underscore) makes it private to its class.
+  final List<ChatMessage> _messages = <ChatMessage>[];
   final TextEditingController _textController = new TextEditingController();
 
+  void _handleSubmitted(String text) {
+    _textController.clear();
+    ChatMessage message = new ChatMessage(
+      text: text,
+    );
+    setState(() {
+      _messages.insert(0, message);
+    });
+  }
+
   Widget _buildTextComposer() {
-    return new IconTheme(
-      data: new IconThemeData(color: Theme.of(context).accentColor),
-      child: new Container(
-      margin: const EdgeInsets.symmetric(horizontal: 8.0),
-        child: new Row(
+   return new Container(
+       margin: const EdgeInsets.symmetric(horizontal: 8.0),
+       child: new Row(
           children: <Widget>[
             new Flexible(
               child: new TextField(
                 controller: _textController,
                 onSubmitted: _handleSubmitted,
                 decoration: new InputDecoration.collapsed(
-                  hintText: "Send a message"),
+                hintText: "Send a message"),
               ),
             ),
             new Container(
@@ -46,24 +87,33 @@ class ChatScreenState extends State<ChatScreen> {
               child: new IconButton(
                 icon: new Icon(Icons.send),
                 onPressed: () => _handleSubmitted(_textController.text)),
-            ),
-          ],
-        ),
-      ),
+           ),
+         ]
+       )
     );
   }
 
-  void _handleSubmitted(String text) {
-    _textController.clear();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-          title: new Text("Friendlychat")
-      ),
-      body: _buildTextComposer(),
-    );
+      appBar: new AppBar(title: new Text("Friendlychat")),
+      body: new Column(
+        children: <Widget>[
+        new Flexible(
+          child: new ListView.builder(
+            padding: new EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
+          )
+        ),
+        new Divider(height: 1.0),
+        new Container(
+          decoration: new BoxDecoration(
+            color: Theme.of(context).cardColor),
+          child: _buildTextComposer(),
+        ),
+       ]
+     ),
+   );
   }
 }
